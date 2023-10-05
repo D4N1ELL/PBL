9734a6dcb5f742c2b347eed0a6efa64c793ac665
Berco Andrei <andrei2003berco@gmail.com>
2023-10-05 15:24:19 +0300

    show hotspots from database

--- App.js
+++ App.js
@@ -5,32 +5,46 @@ import useNetInfo from "@react-native-community/netinfo";
 import { render } from 'react-dom';
 
 
-// useEffect(() => {
-//   Alert.alert('Connection lost', 'Could not connect to a network, please try again', [
-//     {
-//       text: 'Retry',
-//       onPress: () => console.log('Retry Pressed'),
-//       style: 'retry',
-//     },
-//     {
-//       text: 'OK', 
-//       onPress: () => console.log('OK Pressed')
-//     },
-//   ]);
-// }, [state.isConnected]);
-
-
 export default function App() {
-  //const netInfo = useNetInfo();  
-  return(
-    
-      <MapView provider={PROVIDER_GOOGLE} style={{ ...StyleSheet.absoluteFillObject }}>
-      </MapView>
-  
+  const [hotspots, setHotspots] = useState([]);
 
+  useEffect(() => {
+    // Fetch the hotspot data
+    fetch("http://49.13.85.200:8080/hotspots")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((responseJson) => {
+        setHotspots(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle the error (e.g., show an error message)
+      });
+  }, []);
+
+  return (
+    <MapView provider={PROVIDER_GOOGLE} style={{ ...StyleSheet.absoluteFillObject }}>
+      {/* Map over the hotspots array and create a marker for each hotspot */}
+      {hotspots.map((hotspot) => (
+        <Marker
+          key={hotspot.hotspot_id}
+          coordinate={{
+            latitude: hotspot.latitude,
+            longitude: hotspot.longitude,
+          }}
+          title={hotspot.title}
+          description={hotspot.description}
+        />
+      ))}
+    </MapView>
   );
-    
 }
+
+
 
 //React native styles
 const styles = StyleSheet.create({
