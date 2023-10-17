550b11c1d612e6e3c68f1c30365b809ca4bcfe09
Berco Andrei <andrei2003berco@gmail.com>
2023-10-17 10:39:20 +0300

    fix lags

--- components/Map.js
+++ components/Map.js
@@ -1,13 +1,15 @@
 import { StyleSheet , Text, View,Image} from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, memo } from 'react';
 import MapView, { Marker, PROVIDER_GOOGLE , Callout} from 'react-native-maps';
 import { WebView } from 'react-native-webview';
 
 export default function Map(props) {
   const [hotspots, setHotspots] = useState([]);
   const [selectedHotspot, setSelectedHotspot] = useState(null);
+  const [tvc, setTvc] = useState(false);
 
   useEffect(() => {
+    interval = setInterval(() => {
     // Fetch the hotspot data
     fetch("http://49.13.85.200:8080/hotspots")
       .then((response) => {
@@ -17,14 +19,20 @@ export default function Map(props) {
         return response.json();
       })
       .then((responseJson) => {
+        console.log("b4", hotspots.length)
         setHotspots(responseJson);
+        console.log("aftr", hotspots)
       })
       .catch((error) => {
         console.error(error);
         // Handle the error (e.g., show an error message)
       });
-  }, []);
-
+      setTvc(true)
+      setTimeout(()=>setTvc(false), 200)
+    }, 2000)
+    return () => {clearInterval(interval)}
+  }, [hotspots]);
+  console.log("redraw")
   return (
     <MapView
       provider={PROVIDER_GOOGLE} 
@@ -32,14 +40,14 @@ export default function Map(props) {
       onLongPress={props.lph}
     >
       {/* Map over the hotspots array and create a marker for each hotspot */}
-      {hotspots.map((hotspot) => (
+      {hotspots.map((hotspot, index) => (
         <Marker
-          key={hotspot.hotspot_id}
+          key={index}
           coordinate={{
             latitude: hotspot.latitude,
             longitude: hotspot.longitude,
           }}
-          onPress={() => setSelectedHotspot(hotspot)}
+          tracksViewChanges={tvc}
         >
           
           {/* Custom marker component */}
@@ -51,7 +59,7 @@ export default function Map(props) {
   );
 }
 
-const CustomMarker = ({ hotspot }) => {
+const CustomMarker = memo(({ hotspot }) => {
   return (
     <View style={markerStyle}>
       {/* Title */}
@@ -59,10 +67,10 @@ const CustomMarker = ({ hotspot }) => {
       {/* Description */}
       <Text style={descriptionStyle}>{hotspot.description}</Text>
       {/* Image */}
-      <Image source={{ uri: hotspot.imageUrl }} style={imageStyle} />
+      <Image source={{ uri: `http://49.13.85.200:8080/static/${hotspot.hotspot_id}/${hotspot.photos?.at(0)}` }} style={imageStyle} />
     </View>
   );
-};
+});
 
 const markerStyle = {
   width: 100,
@@ -70,10 +78,10 @@ const markerStyle = {
   backgroundColor: 'white',
   padding: 10,
   borderRadius: 5,
-  shadowColor: 'black',
-  shadowOffset: { width: 0, height: 2 },
-  shadowOpacity: 0.2,
-  shadowRadius: 2,
+  // shadowColor: 'black',
+  // shadowOffset: { width: 0, height: 2 },
+  // shadowOpacity: 0.2,
+  // shadowRadius: 2,
 };
 
 const titleStyle = {
