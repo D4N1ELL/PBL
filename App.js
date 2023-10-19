e99c00a16b6e902271b023c43e54e13b043cff73
Andreea Burdui <andreea.burdui@gmail.com>
2023-10-19 13:27:45 +0300

    add bottomsheet but wrong

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { useState } from 'react';
 import { installWebGeolocationPolyfill } from 'expo-location';
+import DetailedHotspot from './components/DetailedHotspot';
 
 
 export default function App() {
@@ -12,16 +13,27 @@ export default function App() {
   const [createPinOpen, setCreatePinOpen] = useState(false)
   const [createPinCoords, setCreatePinCoords] = useState(false)
 
+  const [selectedHotspot, setSelectedHotspot] = useState(null);
+
   const mapPressHandler = (event) => {
       console.log('Long pressed on map')
       setCreatePinOpen(true)
       setCreatePinCoords(event.nativeEvent.coordinate)
   }
+  const markerPressHandler = (hotspot) => (event) => {
+    console.log('Pressed on marker')
+    setSelectedHotspot(hotspot)
+}
 
   return(
     <GestureHandlerRootView style={{ ...StyleSheet.absoluteFillObject }}>
-      <Map longPressHandler={mapPressHandler}/>
+      <Map longPressHandler={mapPressHandler}
+           onMarkerPress={markerPressHandler} />
+           
       <CreatePin isOpen={createPinOpen} setIsOpen={setCreatePinOpen} coords={createPinCoords}/>
+
+      <DetailedHotspot
+      hotspot={selectedHotspot} setHotspot={setSelectedHotspot}/>
     </GestureHandlerRootView>
         
   );
--- components/detailedhotspot.js
+++ components/detailedhotspot.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
-//import BottomSheet from  
+import React, { useCallback, useState, useRef, useEffect, useMemo } from 'react';
+import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, Text, Button, StyleSheet} from 'react-native'; 
+import MapView, { Marker } from 'react-native-maps';
 
-export default function DetailedHotspot() {
+export default function DetailedHotspot(props) {
 
+  const sheetRef = useRef(null);
+
+  const snapPoints = useMemo(() => ['25%', '50%', '75%'], []);
+  
+  useEffect(()=>{
+    console.log(props.hotspot)
+    if (props.hotspot) {
+      sheetRef.current.snapToIndex(0)
+    } else {
+      sheetRef.current.close()
+    }
+  }, [props.hotspot, sheetRef])
+  
 
   return (
-    <>
+    <View style = {styles.bottomSheetContent}>
+    <BottomSheet
+      ref={sheetRef}
+      index={-1}
+      snapPoints={snapPoints}
+      borderRadius={10}
+      enablePanDownToClose
+      keyboardBlurBehavior={"restore"}
+      onClose={()=>props.setHotspot(null)}
+   
+    >
+      <BottomSheetView>
+      <Text>Awesome 🔥 {props.hotspot?.title}</Text>
+      </BottomSheetView>
+      
     
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  DetailedHotspotContainer: {
-    width: '100%',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    position: 'absolute',
-    borderRadius: 25,
-
-  },
-
-});
+        {/* <Button title="Test Button" onPress={() => console.log("Test button pressed")} /> */}
+        </BottomSheet>
+      </View>
+  );};
+  
+  const styles = StyleSheet.create({
+    bottomSheetContent: {
+      backgroundColor: 'white',
+      padding: 16,  //space between its content and its border
+      height:200,
+    },
+    itemContainer:{
+      padding: 6,
+      margin: 6,
+      backgroundColor: "#eee",
+    }
+  })
+  ;
