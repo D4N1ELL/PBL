6c63e2512b327414aa5a6d2128df49a666924aca
Andreea Burdui <andreea.burdui@gmail.com>
2023-10-23 20:58:07 +0300

    added bottomsheet pics and button

--- components/detailedhotspot.js
+++ components/detailedhotspot.js
@@ -1,15 +1,12 @@
-import React, { useCallback, useState, useRef, useEffect, useMemo } from 'react';
+import React, { useRef, useEffect} from 'react';
 import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { View, Text, Button, StyleSheet, Image} from 'react-native'; 
-import MapView, { Marker } from 'react-native-maps';
+import { View, Text, StyleSheet, Image} from 'react-native'; 
 
 export default function DetailedHotspot(props) {
 
   const sheetRef = useRef(null);
 
-  // const snapPoints = useMemo(() => , []);
-  
   useEffect(()=>{
     console.log(props.hotspot)
     if (props.hotspot) {
@@ -18,36 +15,59 @@ export default function DetailedHotspot(props) {
       sheetRef.current.close()
     }
   }, [props.hotspot, sheetRef])
-  
 
   return ( 
     <BottomSheet
       ref={sheetRef}
       index={-1}
-      snapPoints={['25%', '50%', '75%']}
+      snapPoints={['30%', '50%', '75%']}
       borderRadius={10}
       enablePanDownToClose
       keyboardBlurBehavior={"restore"}
       onClose={()=>props.setHotspot(null)}
-   
     > 
     { props.hotspot ? <BottomSheetView>
-      <Text>Awesome 🔥 {props.hotspot?.title}</Text>
       <Image
-                        marginLeft={10}
-                        marginTop={10}
-                        width={100}
-                        height={60}
-                        borderRadius={15}
-                        overflow="hidden"
-                        source={{uri: `http://49.13.85.200:8080/static/${props.hotspot.hotspot_id}/${props.hotspot.photos?.at(0)}`}}
-                    />
-                    </BottomSheetView>
-    : null }
+        style={styles.mainImage}
+        source={{uri: `http://49.13.85.200:8080/static/${props.hotspot.hotspot_id}/${props.hotspot.photos?.at(0)}`}} 
+        />
       
-    
-        {/* <Button title="Test Button" onPress={() => console.log("Test button pressed")} /> */}
-        </BottomSheet>
+      <View >
+        <Text style={styles.hotspotTitle}>          
+          {props.hotspot?.title}
+        </Text>
+
+        <Text style={styles.hotspotDescription}>
+          {props.hotspot?.description}
+        </Text>
+
+        <View style={styles.imageContainer}>
+          <View style = {{flexdirection: 'row'}}> 
+          {props.hotspot?.photos?.map((item, index) => ( //returning jsx with ()
+          
+          // load all pictures on the location
+          <Image 
+          key={index}
+          style={styles.image}
+          source={{uri: `http://49.13.85.200:8080/static/${props.hotspot.hotspot_id}/${props.hotspot.photos}`}}
+          />
+          ))}
+         </View>
+       </View>
+        
+         <View style={styles.bottomSheetContent}>
+          {/* onPress to open creating pin */}
+          <TouchableOpacity >
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Add photos</Text>
+          </View>
+          </TouchableOpacity>
+          </View>
+      </View>
+
+        </BottomSheetView>
+    : null }
+    </BottomSheet>
   );};
   
   const styles = StyleSheet.create({
@@ -60,6 +80,52 @@ export default function DetailedHotspot(props) {
       padding: 6,
       margin: 6,
       backgroundColor: "#eee",
+    },
+    mainImage:{
+      marginLeft: 11,
+      marginTop: 5,
+      width: 390,
+      height: 161,
+      borderRadius: 15,
+      overflow: "hidden",
+    },
+    hotspotTitle:{
+      marginLeft: 10,
+      marginTop: 5,
+      fontSize: 20,
+      color: 'rgba(109, 27, 137, 1)',
+      
+    },
+    hotspotDescription:{
+      marginLeft: 10,
+      marginTop: 5,
+      fontSize: 15,
+      color: 'rgba(0,0,0,0.9)',
+    },
+    imageContainer:{
+      marginVertical: 20,
+      width: '100%',
+      backgroundColor: '#D9D9D9',
+    },
+    image:{
+      height: 120,
+      width: 120,
+      marginTop: 10,
+      borderRadius: 7,
+      marginHorizontal: 10,
+      marginVertical: 8,
+    },
+    buttonContainer:{
+      flexDirection: 'column',
+      height: 50,
+      marginHorizontal: 10,
+      backgroundColor: 'rgba(0,255, 0, 0.5)',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 50,
+    },
+    buttonText: {
+      color: 'rgba(0,0,0,0.9)',
+      fontSize: 20,
     }
-  })
-  ;
+  });
